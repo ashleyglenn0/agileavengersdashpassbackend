09aa8cf1bec4d46a0b00af9211384184ace05a79
Ashley <arglenn023@gmail.com>
2024-11-25 19:22:25 -0600

    Configured JawsDB for Heroku

--- Procfile
+++
@@ -1,3 +0,0 @@
-web: ./mvnw clean install && java -Dserver.port=$PORT -jar target/southwest-dashpass-0.0.1-SNAPSHOT.jar
-
-
--- src/main/java/agileavengers/southwest_dashpass/SouthwestDashpassApplication.java
+++ src/main/java/agileavengers/southwest_dashpass/SouthwestDashpassApplication.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
